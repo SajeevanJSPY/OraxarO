930f73f439bf0b4f2186c953e262e7e10e46a826
sajeevan <sajeevanjspy@gmail.com>
2025-11-02 10:43:54 +0530

    refactor: enhance Oracle connection handling - added additional validation for connection attributes - updated config to use global ConfigManager for centralized access

--- src/lib/db/connection.ts
+++ src/lib/db/connection.ts
@@ -1,24 +1,23 @@
 import chalk from 'chalk';
 import oracledb from 'oracledb';
 
-import { config } from '@lib/config';
+import { config } from '@lib/globals';
 
-let connection: oracledb.Connection;
+let connection: oracledb.Connection | null = null;
 
 export async function connect() {
+    if (connection) throw new Error('already connected to the oracle database');
     if (!config.connection) throw new Error('connection information is not available');
 
-    console.log(
-        chalk.grey(`connecting to the oracle database: ${config.connection.connectString}`),
-    );
     connection = await oracledb.getConnection(config.connection);
-    console.log(chalk.greenBright(`Successfully connected to Oracle Database`));
+    console.log(chalk.grey(`connected to oracle database:  ${config.connection.connectString}`));
 }
 
 export async function close() {
     if (!connection) throw new Error('there is no connection to close');
 
     connection.close();
+    connection = null;
     console.log(
         chalk.grey(
             `oracle database connection closed${config.connection ? ' for ' + config.connection.connectString : ''}`,
@@ -27,5 +26,6 @@ export async function close() {
 }
 
 export function getConnection() {
+    if (!connection) throw new Error('not connected to oracle database');
     return connection;
 }
